f7a5327238f1e4a24efe5b0a84919167ba3add41
xBLADEx <javablood@hotmail.com>
2014-07-28 17:55:36 -0600

    Mobile Nav 5.3.11
    Added Mobile Nav

--- header.php
+++ header.php
@@ -17,12 +17,17 @@
 </head>
 <body <?php body_class(); ?>>
 	<main id="websiteContainer">
+		<div class="mobileBackground">
+			<p class="mobileIdentity"><?php bloginfo( 'name' ); ?></p>
+			<a class="menuIcon" href="#"><span></span><span></span><span></span>Menu</a>
+		</div>
+		<div class="mobileMenu"></div>
 		<header class="headerBackground">
 			<div class="row">
-				<div class="medium-3 columns">
+				<div class="medium-3 small-12 columns">
 					<a href="/" class="logo"><img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt=""></a>
 				</div>
-	            <div class="medium-9 columns">
+	            <div class="hide-for-small medium-9 columns">
 	            	<?php //get_template_part('templates/form', 'search'); ?>
 	                <nav class="mainNav" role="navigation">
 	                    <?php get_template_part('templates/menu', 'nav'); ?>
--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function($){
-
-	
+	// Create Mobile Menu
+	var mobileMenu = $('.mainNav').clone();
+	mobileMenu.appendTo('.mobileMenu');
+	// Toggle Moblie Menu
+	$('a.menuIcon').on('click', function(e) {
+		e.preventDefault();
+		$('.mobileMenu').slideToggle('fast');
+	});
+	// Initialize Foundation
 	$(document).foundation();
 });
