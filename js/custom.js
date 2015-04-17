7cd8b43bc6b02385edf55ded9521d2a1333d7096
xBLADEx <javablood@hotmail.com>
2015-04-17 09:13:28 -0600

    5.5.1.8 - Browser Detect
    Added a check for out dated browser.

--- js/custom.js
+++ js/custom.js
@@ -36,3 +36,49 @@ jQuery(document).ready(function($){
 	// Initialize Foundation
 	$(document).foundation();
 });
+
+var BrowserDetect = {
+	init: function () {
+		this.browser = this.searchString(this.dataBrowser) || "Other";
+		this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || "Unknown";
+	},
+	searchString: function (data) {
+		for (var i = 0; i < data.length; i++) {
+			var dataString = data[i].string;
+			this.versionSearchString = data[i].subString;
+
+			if (dataString.indexOf(data[i].subString) !== -1) {
+				return data[i].identity;
+			}
+		}
+	},
+	searchVersion: function (dataString) {
+		var index = dataString.indexOf(this.versionSearchString);
+		if (index === -1) {
+			return;
+		}
+
+		var rv = dataString.indexOf("rv:");
+		if (this.versionSearchString === "Trident" && rv !== -1) {
+			return parseFloat(dataString.substring(rv + 3));
+		} else {
+			return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
+		}
+	},
+
+	dataBrowser: [
+	{string: navigator.userAgent, subString: "Chrome", identity: "Chrome"},
+	{string: navigator.userAgent, subString: "MSIE", identity: "Explorer"},
+	{string: navigator.userAgent, subString: "Trident", identity: "Explorer"},
+	{string: navigator.userAgent, subString: "Firefox", identity: "Firefox"},
+	{string: navigator.userAgent, subString: "Safari", identity: "Safari"},
+	{string: navigator.userAgent, subString: "Opera", identity: "Opera"}
+	]
+
+};
+
+BrowserDetect.init();
+if ( BrowserDetect.browser == 'Explorer' && BrowserDetect.version <= 9 ) {
+	document.write("<p class='browser-banner'>You are using an outdated browser. Please <a href='http://browsehappy.com/' target='_blank'>upgrade your browser</a> to improve your experience.</p>");
+}
+//document.write("You are using <b>" + BrowserDetect.browser + "</b> with version <b>" + BrowserDetect.version + "</b>");
