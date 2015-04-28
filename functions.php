031dc176023be91e2b4687b94fd849fce4349692
xBLADEx <javablood@hotmail.com>
2015-04-28 14:48:44 -0600

    5.5.1.10 - WP 4.2 Remove Emoji
    Removed the emoji from the header. Removed the /feed links from the
    header. Adjusted focus and link colors.

--- functions.php
+++ functions.php
@@ -66,6 +66,7 @@ if ( ! function_exists( 'foundation_assets' ) ) {
 	add_action( 'wp_enqueue_scripts', 'foundation_assets' );
 }
 // Header Clean up
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Removes the type="application/rss+xml" /feed link
 remove_action( 'wp_head', 'wp_generator' ); // This removes the WordPress version
 remove_action( 'wp_head', 'rsd_link' ); // This removes Windows Live Writer (WLW)
 remove_action( 'wp_head', 'wlwmanifest_link' ); // This removes Windows Live Writer (WLW)
@@ -84,6 +85,11 @@ function remove_recent_comments_style() { // Remove recent comments style tag
     remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );  
 }
 add_action( 'widgets_init', 'remove_recent_comments_style' );
+// 4.2 WordPress Emoji
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 // Create pagination
 if ( ! function_exists( 'blade_pagination' ) ) {
 	function blade_pagination() {
