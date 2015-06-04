253a5bf00ea1d0d25d46541c55a7e0fffb4eb10e
xBLADEx <javablood@hotmail.com>
2015-06-04 11:49:58 -0600

    5.5.1.14 - Contact Form
    Added new Theme Options under admin settings.
    Updated contact form, improved naming convention to php, added new theme
    options option into the email to field.
    Added CSS for contact form.
    Added labels on front end and improved placeholder context.

--- functions/admin.php
+++ functions/admin.php
@@ -64,3 +64,105 @@ if ( ! function_exists( 'custom_login_logo' ) ) {
 	}
 	add_action( 'login_enqueue_scripts', 'custom_login_logo' );
 }
+/* 
+====================
+	THEME OPTIONS
+====================
+*/
+class Custom_Theme_Options {
+
+	public $options;
+
+	public function __construct() {
+		//$this->options = delete_option( 'custom_theme_setting' );
+		$this->options = get_option( 'custom_theme_setting' );
+		$this->register_settings_fields();
+	}
+
+	public function add_menu_page() {
+		add_options_page( // https://codex.wordpress.org/Function_Reference/add_menu_page
+			'Theme Options', 	// Page Title
+			'Theme Options', 	// Menu Title
+			'manage_options',	// Capability 
+			'theme-options',	// Slug
+			array( 'Custom_Theme_Options', 'display_options_page' ) // Function
+		); 
+	}
+
+	// Add Menu Page - Options Page
+	public function display_options_page() {
+		?>
+		<div class="wrap">
+			<h2>Theme Options</h2>
+			<form action="options.php" method="POST">
+				<?php 
+					settings_fields( 'custom_theme_setting' ); // register_setting() 1st arg Group Name. 
+					do_settings_sections( 'theme-options' );
+					submit_button();
+				?>
+			</form>
+		</div>
+		<?php
+	}
+
+	public function register_settings_fields() {
+		
+		register_setting( // 1 & 2. Codex says use same name, 3. Optional - Used for sanitizing / validating
+			'custom_theme_setting', // Group
+			'custom_theme_setting',	// Name - Used with get_option() / update_option()
+			array( $this, 'custom_validation' )
+		); // https://codex.wordpress.org/Function_Reference/register_setting
+		
+		add_settings_section( // 1. ID, 2. Title displayed within <h3>, 3. Callback, 4. Page - Needs to match the slug url
+			'rich_main_section',
+			'Theme Settings',
+			array( $this, 'rich_main_section_cb' ),
+			'theme-options'
+		); 
+
+		add_settings_field( // 1. Name heading or title also ID, 3. Callback, 4. Page, 5. Section - add_settings_section 1st arg.
+			'theme_email_heading',
+			'Contact Form Email: ',
+			array( $this, 'theme_email_field_cb' ), 
+			'theme-options', 
+			'rich_main_section'
+		);
+
+	}
+
+	// CALLBACKS
+	public function custom_validation( $input ) {
+		//$input['custom_theme_setting'] = strip_tags( stripslashes( $input['custom_theme_setting'] ) );
+		//return $input;
+
+		// Create new array() to store options
+		$output = array();
+		// Loop and check all options
+		foreach ( $input as $key => $value ) {
+			// If option has value, proceed
+			if ( isset( $input[$key] ) ) {
+				// Clean HTML tags
+				$output[$key] = strip_tags( stripslashes( $input[$key] ) );
+			}
+		}
+		return $output;
+	}
+
+	public function rich_main_section_cb() {
+		echo 'Input your email for the contact form. For multiple emails use a comma to separate.';
+	}
+
+	// Email Field
+	public function theme_email_field_cb() {
+		echo "<input class='regular-text' name='custom_theme_setting[theme_email_heading]' type='text' value='{$this->options['theme_email_heading']}' >";
+	}
+	
+}
+
+add_action( 'admin_menu', function() {
+	Custom_Theme_Options::add_menu_page(); // The double colon :: calls a function without instantiating the entire class
+});
+
+add_action( 'admin_init', function() { 
+	new Custom_Theme_Options(); // This will automatically call the __construct() function.
+});
--- templates/form-contact.php
+++ templates/form-contact.php
@@ -8,17 +8,17 @@
 <?php 
 if ( isset( $_POST['submit'] ) ) { 
 	// Define emails, website, and domain.
-	define( 'EMAIL',	'rich@sorensonadvertising.com' );
+	define( 'EMAIL',	get_option( 'custom_theme_setting' )['theme_email_heading'] );
 	define( 'WEBSITE',	get_bloginfo( 'name' ) );
 	define( 'DOMAIN',	str_replace( "www.", "", $_SERVER['HTTP_HOST'] ) );
 	// If trap value is set redirect to home page.
-	if ( $_POST['mainAddress'] != '' ) {
+	if ( $_POST['main_address'] != '' ) {
 		echo '<html><head><meta http-equiv="refresh" content="0; url=/"></head><body></body></html>';
 		exit;
 	}
 	// Set required fields
 	$required_fields = array(
-		'fullName',
+		'full_name',
 		'email',
 		'phone'
 	);
@@ -30,13 +30,13 @@ if ( isset( $_POST['submit'] ) ) {
 		}
 	}
 	// Grab form fields
-	$fullName 	= $_POST["fullName"];
+	$full_name 	= $_POST["full_name"];
 	$email 		= $_POST["email"];
 	$phone 		= $_POST["phone"];
 	$comment 	= $_POST["comment"];
 	// Sanitize Information
-	if ( $fullName != '' ) {
-		$fullName = filter_var( $fullName, FILTER_SANITIZE_STRING );
+	if ( $full_name != '' ) {
+		$full_name = filter_var( $full_name, FILTER_SANITIZE_STRING );
 	}
 	if ( $email != '' ) {
 		$email = filter_var( $email, FILTER_SANITIZE_EMAIL );
@@ -46,15 +46,20 @@ if ( isset( $_POST['submit'] ) ) {
 		$phone = preg_replace( $regex, '$1-$2-$3', $phone ); // http://php.net/manual/en/function.preg-match.php
 	}
 	// Set mail() variables http://php.net/manual/en/function.mail.php
-	$to 		= EMAIL;
-	$subject 	= WEBSITE . ' Contact Form'. "\r\n";
+	// Check if theme options email field is set
+	if ( EMAIL == '' ) {
+		$to = 'rich@sorensonadvertising.com';
+	} else {
+		$to 	= EMAIL;
+	}
+	$subject 	= WEBSITE . ' Contact Form' . "\r\n";
 	$headers	= 'MIME-Version: 1.0' . "\r\n";
 	$headers	.= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 	$headers  	.= "From: contact@" . DOMAIN . "\r\nReply-To: noreply@" . DOMAIN . "\r\n";
 	$message 	= '<html><body>';
 	$message 	.= '<table rules="all" style="border-color: #B8B8B8;" cellpadding="10">';
-	$message 	.= '<tr style="background: #5979FF; color: #FFF; font-weight:bold;"><th>FIELD</th><th>INFORMATION</th></tr>';
-	$message 	.= "<tr><td><strong>Name:</strong></td><td>" . strip_tags( $fullName ) . "</td></tr>";
+	$message 	.= '<tr style="background: #5979FF; color: #FFF; font-weight: bold;"><th>FIELD</th><th>INFORMATION</th></tr>';
+	$message 	.= "<tr><td><strong>Name:</strong></td><td>" . strip_tags( $full_name ) . "</td></tr>";
 	$message 	.= "<tr><td><strong>Email:</strong></td><td>" . strip_tags( $email ) . "</td></tr>";
 	$message 	.= "<tr><td><strong>Phone:</strong></td><td>" . strip_tags( $phone ) . "</td></tr>";
 	$message 	.= "<tr><td><strong>Comment:</strong></td><td>" . strip_tags( $comment ) . "</td></tr>";
@@ -69,16 +74,28 @@ if ( isset( $_POST['submit'] ) ) {
 	} else {
 		echo '<p class="alert-box alert">Please fill out the required fields.</p>';
 	}
+
+} else { 
 ?>
-<?php
-} else { ?>
-	<form method="POST">
-		<input type="text" name="fullName" placeholder="Full Name" required>
-		<input type="email" name="email" placeholder="Email" required>
-		<input type="tel" maxlength="12" name="phone" pattern="\d{3}[\-]?\d{3}[\-]?\d{4}" placeholder="Phone" required>
-		<textarea name="comment" placeholder="Message"></textarea>
-		<input type="text" name="mainAddress" value="" class="hidden">
-		<input type="submit" name="submit" value="Submit" class="button">
-	</form>
+<form method="POST">
+	<label>
+		Full Name
+		<input type="text" name="full_name" placeholder="John Doe" required>
+	</label>
+	<label>
+		Email
+		<input type="email" name="email" placeholder="john@example.com" required>
+	</label>
+	<label>
+		Phone
+		<input type="tel" maxlength="12" name="phone" pattern="\d{3}[\-]?\d{3}[\-]?\d{4}" placeholder="555-555-5555" required>
+	</label>
+	<label>
+		Questions or Comments
+		<textarea class="form-comment" name="comment" placeholder="What's on your mind?"></textarea>
+	</label>
+	<input type="text" name="main_address" value="" class="hidden">
+	<input type="submit" name="submit" value="Submit" class="button">
+</form>
 <?php
 } ?>
