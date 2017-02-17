078fa93e9f2aa78212bb7a9a9af98a529f15f2ff
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 12:28:19 -0700

    Escape attribute for search inputs for form-search.php.

--- themes/custom/templates/form-search.php
+++ themes/custom/templates/form-search.php
@@ -6,9 +6,11 @@
  */
 
 ?>
+
 <div class="clearfix">
 	<form action="/" class="form-search" method="GET">
-		<input type="submit" class="form-search-button" name="submit" value="Find">
-		<input type="text" class="form-search-box" name="s" placeholder="Search">
+		<input type="submit" class="form-search-button" name="submit" value="<?php esc_attr_e( 'Find', 'custom' ); ?>">
+
+		<input type="text" class="form-search-box" name="s" placeholder="<?php esc_attr_e( 'Search', 'custom' ); ?>">
 	</form>
 </div>
