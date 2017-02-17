95b2c9641fe7cc98130fc69fb4f32bbe8df53ce1
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 12:35:51 -0700

    Adjust spacing and switch PHP brackets to end notation.

--- themes/custom/templates/page-full-width.php
+++ themes/custom/templates/page-full-width.php
@@ -7,20 +7,26 @@
 
 get_header();
 ?>
+
 <div class="page-content">
 	<div class="row">
 		<div class="medium-12 columns">
 			<?php get_template_part( 'templates/page', 'title' ); ?>
 		</div>
 	</div>
+
 	<?php
-	if ( have_posts() ) {
-		while ( have_posts() ) { the_post(); ?>
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
+			?>
 			<div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<?php the_content(); ?>
 			</div>
-		<?php
-		}
-	} ?>
+			<?php
+		endwhile;
+	endif;
+	?>
 </div>
+
 <?php get_footer(); ?>
