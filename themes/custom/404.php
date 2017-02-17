83dda3868832433571baa64214e3c30608529c45
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 09:36:47 -0700

    Escape text for 404 page.

--- themes/custom/404.php
+++ themes/custom/404.php
@@ -9,8 +9,8 @@ get_header();
 ?>
 <div class="row page-content">
 	<div class="medium-12 columns">
-		<h1>404 Page Not Found!</h1>
-		<p>This page no longer exists.</p>
+		<h1><?php esc_html_e( '404 Page Not Found!', 'custom' ); ?></h1>
+		<p><?php esc_html_e( 'This page no longer exists.', 'custom' ); ?></p>
 	</div>
 </div>
 <?php get_footer(); ?>
