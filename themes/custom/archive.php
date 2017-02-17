ed84f892712e4b1a86bab63fc8550de83116b9e6
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 10:12:17 -0700

    Adjust indenting of php for archive.php.

--- themes/custom/archive.php
+++ themes/custom/archive.php
@@ -14,7 +14,9 @@ get_header();
 	<div class="medium-9 columns">
 		<?php
 		if ( have_posts() ) :
-			while ( have_posts() ) : the_post(); ?>
+			while ( have_posts() ) :
+				the_post();
+				?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header>
 						<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
@@ -34,11 +36,12 @@ get_header();
 						<p class="tags"><?php the_tags( '<span>', '</span><span>', '</span>' ); ?></p>
 					</footer>
 				</article>
-			<?php
+				<?php
 			endwhile;
 
 			blade_pagination();
-		endif; ?>
+		endif;
+		?>
 	</div>
 
 	<aside class="medium-3 columns">
