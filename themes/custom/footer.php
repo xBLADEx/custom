2d43d71a535a2e025db29cc9a9bcd27c527e6e40
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 10:31:13 -0700

    Escape text and add spacing for footer.php.

--- themes/custom/footer.php
+++ themes/custom/footer.php
@@ -6,14 +6,18 @@
  */
 
 ?>
+
 </main>
+
 <footer class="footer">
 	<div class="row">
 		<div class="medium-12 columns">
-			<p>Copyright &copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. All Rights Reserved.</p>
+			<p><?php esc_html_e( 'Copyright &copy;', 'custom' ); ?> <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'All Rights Reserved.', 'custom' ); ?></p>
 		</div>
 	</div>
 </footer>
+
 <?php wp_footer(); ?>
+
 </body>
 </html>
