fbedca1aa0d70bb80e5d8957dbe2f93cc4f3be04
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 14:07:47 -0700

    Remove function exists check, adjust comments.

--- themes/custom/functions/support.php
+++ themes/custom/functions/support.php
@@ -5,22 +5,27 @@
  * @package Custom
  */
 
-if ( ! function_exists( 'custom_support' ) ) {
+/**
+ * Theme Support
+ */
+function custom_support() {
+	// Featured Images.
+	add_theme_support( 'post-thumbnails' );
+
+	// Automatic Feed Links & Post Formats.
+	// add_theme_support( 'automatic-feed-links' );
 
 	/**
-	 * Theme Support
+	 * Post Formats
+	 *
+	 * @see http://codex.wordpress.org/Post_Formats.
 	 */
-	function custom_support() {
-		// Featured Images.
-		add_theme_support( 'post-thumbnails' );
-		// Automatic Feed Links & Post Formats.
-		// add_theme_support( 'automatic-feed-links' );
-		// Post Formats http://codex.wordpress.org/Post_Formats.
-		// add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'audio', 'quote', 'link', 'gallery', 'status' ) );
-	}
-
-	add_action( 'after_setup_theme', 'custom_support' );
+	// add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'audio', 'quote', 'link', 'gallery', 'status' ) );
 }
 
-// Image Size.
+add_action( 'after_setup_theme', 'custom_support' );
+
+/**
+ * Image Sizes
+ */
 // add_image_size( 'name', 150, 80 );
