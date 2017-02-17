38d85404c5029968ff1768fd9c04e5dc6e155103
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 10:10:31 -0700

    Replace  PHP brackets for endif notation for easier reading.

--- themes/custom/archive.php
+++ themes/custom/archive.php
@@ -13,8 +13,8 @@ get_header();
 <div class="row page-content">
 	<div class="medium-9 columns">
 		<?php
-		if ( have_posts() ) {
-			while ( have_posts() ) { the_post(); ?>
+		if ( have_posts() ) :
+			while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header>
 						<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
@@ -35,9 +35,10 @@ get_header();
 					</footer>
 				</article>
 			<?php
-			}
+			endwhile;
+
 			blade_pagination();
-		} ?>
+		endif; ?>
 	</div>
 
 	<aside class="medium-3 columns">
