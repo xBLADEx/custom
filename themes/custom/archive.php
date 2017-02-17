02e5e5bd1b842128875ab0121e92af0a5d7323ab
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 10:08:14 -0700

    Add spacing and missing semi colon for archive.php.

--- themes/custom/archive.php
+++ themes/custom/archive.php
@@ -7,7 +7,9 @@
 
 get_header();
 ?>
+
 <?php get_template_part( 'templates/page', 'title' ); ?>
+
 <div class="row page-content">
 	<div class="medium-9 columns">
 		<?php
@@ -15,14 +17,19 @@ get_header();
 			while ( have_posts() ) { the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header>
-						<h2><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
 						<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+
 						<p class="date">Date: <?php the_time( get_option( 'date_format' ) ); ?></p>
+
 						<p class="categories">Categories: <?php the_category( ', ' ); ?></p>
 					</header>
+
 					<div>
 						<?php the_excerpt(); ?>
 					</div>
+
 					<footer>
 						<p class="tags"><?php the_tags( '<span>', '</span><span>', '</span>' ); ?></p>
 					</footer>
@@ -32,8 +39,10 @@ get_header();
 			blade_pagination();
 		} ?>
 	</div>
+
 	<aside class="medium-3 columns">
 		<?php dynamic_sidebar( 'Sidebar Blog' ); ?>
 	</aside>
 </div>
+
 <?php get_footer(); ?>
