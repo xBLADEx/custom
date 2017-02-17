5ee534b352af2ec8f8729d28a46a9f99e393bcf7
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 13:41:02 -0700

    Add responsive initial scale in header meta viewport.

--- themes/custom/header.php
+++ themes/custom/header.php
@@ -11,7 +11,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '-', true, 'right' ); ?></title>
 	<?php wp_head(); ?>
 </head>
