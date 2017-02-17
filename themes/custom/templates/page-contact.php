11c7bf171a7792622a3234299434a23ea73b3fe4
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 12:34:49 -0700

    Adjust spacing and PHP brackets to end notation.

--- themes/custom/templates/page-contact.php
+++ themes/custom/templates/page-contact.php
@@ -7,25 +7,31 @@
 
 get_header();
 ?>
+
 <div class="row page-content">
 	<div class="medium-12 columns">
 		<?php get_template_part( 'templates/page', 'title' ); ?>
+
 		<div class="row">
 			<div class="medium-6 columns">
 				<?php get_template_part( 'templates/form', 'contact' ); ?>
 			</div>
+
 			<div class="medium-6 columns">
 				<?php
-				if ( have_posts() ) {
-					while ( have_posts() ) { the_post(); ?>
+				if ( have_posts() ) :
+					while ( have_posts() ) :
+						the_post();
+						?>
 						<div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
 							<?php the_content(); ?>
 						</div>
-					<?php
-					}
-				} ?>
+						<?php
+					endwhile;
+				endif; ?>
 			</div>
 		</div>
 	</div>
 </div>
+
 <?php get_footer(); ?>
