0e9fc119cb3eedb5e8b025e02974e7d2a994edab
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 12:27:09 -0700

    Escape text and change brackets to end notation.

--- themes/custom/templates/form-contact.php
+++ themes/custom/templates/form-contact.php
@@ -5,16 +5,16 @@
  * @package Custom
  */
 
-if ( isset( $_POST['submit'] ) ) {
-
+if ( isset( $_POST['submit'] ) ) :
 	// Define emails, website, and domain.
-	define( 'EMAIL',	get_option( 'custom_theme_setting' )['theme_email_heading'] );
-	define( 'WEBSITE',	get_bloginfo( 'name' ) );
-	define( 'DOMAIN',	str_replace( 'www.', '', $_SERVER['HTTP_HOST'] ) );
+	define( 'EMAIL',   get_option( 'custom_theme_setting' )['theme_email_heading'] );
+	define( 'WEBSITE', get_bloginfo( 'name' ) );
+	define( 'DOMAIN',  str_replace( 'www.', '', $_SERVER['HTTP_HOST'] ) );
 
 	// If trap value is set redirect to home page.
 	if ( '' !== $_POST['main_address'] ) {
 		echo '<html><head><meta http-equiv="refresh" content="0; url=/"></head><body></body></html>';
+
 		exit;
 	}
 
@@ -27,6 +27,7 @@ if ( isset( $_POST['submit'] ) ) {
 
 	// Check if required fields are blank.
 	$field_errors = true;
+
 	foreach ( $required_fields as &$field ) {
 		if ( '' === $_POST[$field] ) {
 			$field_errors = false;
@@ -34,10 +35,10 @@ if ( isset( $_POST['submit'] ) ) {
 	}
 
 	// Grab form fields.
-	$full_name 	= $_POST['full_name'];
-	$email 		= $_POST['email'];
-	$phone 		= $_POST['phone'];
-	$comment 	= $_POST['comment'];
+	$full_name = $_POST['full_name'];
+	$email     = $_POST['email'];
+	$phone     = $_POST['phone'];
+	$comment   = $_POST['comment'];
 
 	// Sanitize Information.
 	if ( '' !== $full_name ) {
@@ -53,38 +54,43 @@ if ( isset( $_POST['submit'] ) ) {
 
 	// Check if theme options email field is set.
 	if ( '' === EMAIL ) {
-		$to 	= 'rich@sorensonadvertising.com';
+		$to = 'javablood@hotmail.com';
 	} else {
-		$to 	= EMAIL;
+		$to = EMAIL;
 	}
 
-	$subject 	= WEBSITE . ' Contact Form' . "\r\n";
+	$subject = WEBSITE . ' Contact Form' . "\r\n";
 
-	$headers	= 'MIME-Version: 1.0' . "\r\n";
-	$headers	.= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-	$headers  	.= 'From: contact@' . DOMAIN . "\r\nReply-To: noreply@" . DOMAIN . "\r\n";
+	$headers = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+	$headers .= 'From: contact@' . DOMAIN . "\r\nReply-To: noreply@" . DOMAIN . "\r\n";
 
-	$message 	= '<html><body>';
-	$message 	.= '<table rules="all" style="border-color: #B8B8B8;" cellpadding="10">';
-	$message 	.= '<tr style="background: #5979FF; color: #FFF; font-weight: bold;"><th>FIELD</th><th>INFORMATION</th></tr>';
-	$message 	.= '<tr><td><strong>Name:</strong></td><td>' . strip_tags( $full_name ) . '</td></tr>';
-	$message 	.= '<tr><td><strong>Email:</strong></td><td>' . strip_tags( $email ) . '</td></tr>';
-	$message 	.= '<tr><td><strong>Phone:</strong></td><td>' . strip_tags( $phone ) . '</td></tr>';
-	$message 	.= '<tr><td><strong>Comment:</strong></td><td>' . strip_tags( stripslashes( $comment ) ) . '</td></tr>';
-	$message 	.= '</table>';
-	$message 	.= '</body></html>';
+	$message = '<html><body>';
+	$message .= '<table rules="all" style="border-color: #B8B8B8;" cellpadding="10">';
+	$message .= '<tr style="background: #5979FF; color: #FFF; font-weight: bold;"><th>FIELD</th><th>INFORMATION</th></tr>';
+	$message .= '<tr><td><strong>Name:</strong></td><td>' . strip_tags( $full_name ) . '</td></tr>';
+	$message .= '<tr><td><strong>Email:</strong></td><td>' . strip_tags( $email ) . '</td></tr>';
+	$message .= '<tr><td><strong>Phone:</strong></td><td>' . strip_tags( $phone ) . '</td></tr>';
+	$message .= '<tr><td><strong>Comment:</strong></td><td>' . strip_tags( stripslashes( $comment ) ) . '</td></tr>';
+	$message .= '</table>';
+	$message .= '</body></html>';
 
 	// PHP Rule: Message lines should not exceed 70 characters, so wrap it.
-	$message 	= wordwrap( $message, 70 );
+	$message = wordwrap( $message, 70 );
 
 	// If no errors - send. Else - alert.
-	if ( $field_errors ) {
+	if ( $field_errors ) :
 		wp_mail( $to, $subject, $message, $headers );
-		echo '<p class="alert-box success">Thank you for contacting us!</p>';
-	} else {
-		echo '<p class="alert-box alert">Please fill out the required fields.</p>';
-	}
-} else { ?>
+		?>
+		<p class="alert-box success"><?php esc_html_e( 'Thank you for contacting us!', 'custom' ); ?></p>
+		<?php
+	else :
+		?>
+		<p class="alert-box alert"><?php esc_html_e( 'Please fill out the required fields.', 'custom' ); ?></p>
+		<?php
+	endif;
+else :
+	?>
 	<form method="POST">
 		<label>
 			Full Name
@@ -105,5 +111,5 @@ if ( isset( $_POST['submit'] ) ) {
 		<input type="text" name="main_address" value="" class="hidden">
 		<input type="submit" name="submit" value="Submit" class="button">
 	</form>
-<?php
-} ?>
+	<?php
+endif;
