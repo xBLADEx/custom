cbf9797b49153af94543171f1b587a00334c7507
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 10:29:01 -0700

    Adjust spacing, escape text, replace PHP brackets for comments.php.

--- themes/custom/comments.php
+++ themes/custom/comments.php
@@ -1,47 +1,62 @@
 <?php
 /**
  * Comments
+ * Comments template in functions/post.php foundation_comment().
  *
  * @package Custom
  */
 
-// Comments template in functions/post.php.
-if ( post_password_required() ) { return; }
-?>
-<?php
-if ( have_comments() ) { ?>
+// Exit if post is password protected.
+if ( post_password_required() ) {
+	return;
+}
+
+if ( have_comments() ) :
+	?>
 	<h4 class="comment-title">
-		<?php printf( _n( '1 reply to &ldquo;%2$s&rdquo;', '%1$s replies to &ldquo;%2$s&rdquo;', get_comments_number(), 'foundation' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' ); ?>
+		<?php printf( _n( '1 reply to &ldquo;%2$s&rdquo;', '%1$s replies to &ldquo;%2$s&rdquo;', get_comments_number(), 'custom' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' ); ?>
 	</h4>
+
 	<ol class="comment-list">
 		<?php
-		// See: http://codex.wordpress.org/Function_Reference/wp_list_comments.
+		// @see http://codex.wordpress.org/Function_Reference/wp_list_comments.
 		wp_list_comments(
 			array(
 				'callback' => 'foundation_comment',
 				'style'    => 'ol',
 			)
-		); ?>
+		);
+		?>
 	</ol>
+
 	<?php
-	if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { ?>
+	if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
+		?>
 		<nav>
-			<h4>Comment navigation</h4>
-			<div><?php previous_comments_link( '&larr; Older Comments' ); ?></div>
-			<div><?php next_comments_link( 'Newer Comments &rarr;' ); ?></div>
+			<h4><?php esc_html_e( 'Comment navigation', 'custom' ); ?></h4>
+
+			<div>
+				<?php previous_comments_link( '&larr; Older Comments' ); ?>
+			</div>
+
+			<div>
+				<?php next_comments_link( 'Newer Comments &rarr;' ); ?>
+			</div>
 		</nav>
-	<?php
-	}
+		<?php
+	endif;
 
-	if ( ! comments_open() && get_comments_number() ) { ?>
-		<p>Comments are closed.</p>
-	<?php
-	}
-} // End have_comments().
+	if ( ! comments_open() && get_comments_number() ) :
+		?>
+		<p><?php esc_html_e( 'Comments are closed.', 'custom' ); ?></p>
+		<?php
+	endif;
+endif;
 
-if ( comments_open() ) { ?>
+if ( comments_open() ) :
+	?>
 	<div class="panel comment-form">
-		<?php comment_form(); // See: http://codex.wordpress.org/Function_Reference/comment_form. ?>
+		<?php comment_form(); // @see http://codex.wordpress.org/Function_Reference/comment_form. ?>
 	</div>
-<?php
-} ?>
+	<?php
+endif;
