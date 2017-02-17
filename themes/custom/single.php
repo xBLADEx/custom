cf8077c7ca145640c6839c5a601cb0a554310bd0
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 11:36:01 -0700

    Escape text, adjust spacing, use time element for single.php.

--- themes/custom/single.php
+++ themes/custom/single.php
@@ -7,28 +7,40 @@
 
 get_header();
 ?>
+
 <div class="row page-content">
 	<div class="medium-9 columns">
 		<?php
-		if ( have_posts() ) {
-			while ( have_posts() ) { the_post(); ?>
+		if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post();
+				?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<?php get_template_part( 'templates/page', 'title' ); ?>
+
 					<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-					<p class="date">Date: <?php the_time( get_option( 'date_format' ) ); ?></p>
-					<p class="categories">Categories: <?php the_category( ', ' ); ?></p>
+
+					<p class="date"><?php esc_html_e( 'Date:', 'custom' ); ?> <time><?php the_time( get_option( 'date_format' ) ); ?></time></p>
+
+					<p class="categories"><?php esc_html_e( 'Categories:', 'custom' ); ?> <?php the_category( ', ' ); ?></p>
+
 					<div>
 						<?php the_content(); ?>
+
 						<p class="tags"><?php the_tags( '<span>', '</span><span>', '</span>' ); ?></p>
+
 						<?php comments_template(); ?>
 					</div>
 				</article>
-			<?php
-			}
-		} ?>
+				<?php
+			endwhile;
+		endif;
+		?>
 	</div>
+
 	<aside class="medium-3 columns">
 		<?php dynamic_sidebar( 'Sidebar Blog' ); ?>
 	</aside>
 </div>
+
 <?php get_footer(); ?>
