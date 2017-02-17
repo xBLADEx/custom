cf4c7ce0ddfbb2e7ff5c3de548d1bcbf5c752d0b
Rich Edmunds <rich@webdevstudios.com>
2017-02-17 12:56:39 -0700

    Clean up quicktags and custom quicktags.

--- themes/custom/functions/admin.php
+++ themes/custom/functions/admin.php
@@ -8,57 +8,48 @@
 // Do not allow file edit.
 define( 'DISALLOW_FILE_EDIT', true );
 
+// Insert images default link type to none.
 update_option( 'image_default_link_type', 'none' );
 
-if ( ! function_exists( 'custom_remove_quicktags' ) ) {
+/**
+ * Remove Quicktags
+ *
+ * @param  array $qt_init Element.
+ * @return array          Editor tags.
+ */
+function custom_remove_quicktags( $qt_init ) {
+	// Whatever is in the below string displays in the editor. Important: No spaces after the comma.
+	$qt_init['buttons'] = 'link,ul,ol,li,code';
 
-	/**
-	 * Remove Quicktags
-	 *
-	 *  @param  array $qt_init Element.
-	 * @return array           Editor tags.
-	 */
-	function custom_remove_quicktags( $qt_init ) {
-		// Whatever is in the below string displays in the editor. Important: No spaces after the comma.
-		$qt_init['buttons'] = 'link';
-		return $qt_init;
-	}
-
-	add_filter( 'quicktags_settings', 'custom_remove_quicktags' );
-
+	return $qt_init;
 }
 
-if ( ! function_exists( 'custom_add_quicktags' ) ) {
+add_filter( 'quicktags_settings', 'custom_remove_quicktags' );
 
-	/**
-	 * Add Custom Quicktags
-	 */
-	function custom_add_quicktags() {
-		?>
-		<script>
-			if ( typeof( QTags ) == 'function' ) {
-				QTags.addButton( 'eg_div6', 'div 6', '<div class="medium-6 columns">\n', '\n</div>', 'd', 'Div 6', 1 );
-				QTags.addButton( 'eg_div', 'div', '<div class="">\n', '\n</div>', 'd', 'Division', 1 );
-				QTags.addButton( 'eg_h2', 'H2', '<h2>', '</h2>', '2', 'Heading 2', 1 );
-				QTags.addButton( 'eg_h3', 'H3', '<h3>', '</h3>', '3', 'Heading 3', 1 );
-				QTags.addButton( 'eg_h4', 'H4', '<h4>', '</h4>', '4', 'Heading 4', 1 );
-				QTags.addButton( 'eg_paragraph', 'p', '<p>', '</p>', 'p', 'Paragraph', 1 );
-		        QTags.addButton( 'eg_span', 'span', '<span>', '</span>', 'span', 'Span', 1 );
-		        QTags.addButton( 'eg_bold', 'bold', '<span class="bold">', '</span>', 'bold', 'Bold', 1 );
-		        QTags.addButton( 'eg_italic', 'italic', '<span class="italic">', '</span>', 'italic', 'Italic', 1 );
-				QTags.addButton( 'eg_break', 'br', '<br>', '', 'b', 'Line Break', 20 );
-				QTags.addButton( 'eg_hrule', 'hr', '<hr>\n', '', 'h', 'Horizontal Rule', 20 );
-				QTags.addButton( 'eg_ordered', 'ol', '<ol>\n', '\n</ol>', 'o', 'Ordered List', 20 );
-				QTags.addButton( 'eg_unordered', 'ul', '<ul>\n', '\n</ul>', 'u', 'Unordered List', 20 );
-				QTags.addButton( 'eg_listitem', 'li', '<li>', '</li>', 'l', 'List Item', 20 );
-			}
-		</script>
-		<?php
+/**
+ * Add Custom Quicktags
+ */
+function custom_add_quicktags() {
+	?>
+	<script>
+	if ( typeof( QTags ) == 'function' ) {
+		QTags.addButton( 'eg_div', 'div', '<div class="">\n', '\n</div>', 'd', 'Division', 1 );
+		QTags.addButton( 'eg_h2', 'h2', '<h2>', '</h2>', '2', 'Heading 2', 1 );
+		QTags.addButton( 'eg_h3', 'h3', '<h3>', '</h3>', '3', 'Heading 3', 1 );
+		QTags.addButton( 'eg_h4', 'h4', '<h4>', '</h4>', '4', 'Heading 4', 1 );
+		QTags.addButton( 'eg_paragraph', 'p', '<p>', '</p>', 'p', 'Paragraph', 1 );
+		QTags.addButton( 'eg_span', 'span', '<span>', '</span>', 'span', 'Span', 1 );
+		QTags.addButton( 'eg_bold', 'bold', '<span class="bold">', '</span>', 'bold', 'Bold', 1 );
+		QTags.addButton( 'eg_italic', 'italic', '<span class="italic">', '</span>', 'italic', 'Italic', 1 );
+		QTags.addButton( 'eg_break', 'br', '<br>', '', 'b', 'Line Break', 20 );
+		QTags.addButton( 'eg_hrule', 'hr', '<hr>\n', '', 'h', 'Horizontal Rule', 20 );
 	}
-
-	add_action( 'admin_print_footer_scripts', 'custom_add_quicktags' );
-
+	</script>
+	<?php
 }
+
+add_action( 'admin_print_footer_scripts', 'custom_add_quicktags' );
+
 
 if ( ! function_exists( 'custom_login_logo' ) ) {
 
@@ -68,12 +59,12 @@ if ( ! function_exists( 'custom_login_logo' ) ) {
 	 */
 	function custom_login_logo() {
 		?>
-	    <style>
-	        body.login div#login h1 a {
-	            background-image: url('<?php echo esc_attr( THEME_IMAGES ); ?>/logo-login.jpg');
-	            padding-bottom: 30px;
-	        }
-	    </style>
+		<style>
+			body.login div#login h1 a {
+				background-image: url('<?php echo esc_attr( THEME_IMAGES ); ?>/logo-login.jpg');
+				padding-bottom: 30px;
+			}
+		</style>
 		<?php
 	}
 
