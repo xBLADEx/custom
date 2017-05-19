c84d6071676bae4d50c0b211094a42ebb3f430d9
Rich Edmunds <rich@webdevstudios.com>
2017-05-19 12:40:45 -0600

    Add source link, update jQuery version and jQuery UI version.

--- themes/custom/functions/enqueue.php
+++ themes/custom/functions/enqueue.php
@@ -12,12 +12,13 @@
  * @see http://codex.wordpress.org/Function_Reference/wp_enqueue_script
  * @see https://fonts.google.com/specimen/Lato?selection.family=Lato:300,400,700,900.
  * @see https://fonts.google.com/specimen/Halant?selection.family=Halant:400,600.
+ * @see https://developers.google.com/speed/libraries/#jquery.
  * @example wp_enqueue_style( $handle, $src, $deps, $ver, $media ).
  * @example wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer ).
  */
 function custom_enqueue() {
 	// Styles.
-	// wp_enqueue_style( 'jquery-ui-css', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css', array(), '1.11.4' );
+	// wp_enqueue_style( 'jquery-ui-css', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css', array(), '1.11.4' );
 	// wp_enqueue_style( 'custom', THEME_CSS . '/custom.css', array(), '1.0' );
 
 	// If the cookie is set, load our CSS normally.
@@ -28,8 +29,8 @@ function custom_enqueue() {
 
 	// Scripts.
 	wp_deregister_script( 'jquery' );
-	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js', array(), '2.2.0', true );
-	// wp_enqueue_script( 'jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js', array('jquery'), '1.11.4', true );
+	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2.1', true );
+	// wp_enqueue_script( 'jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js', array('jquery'), '1.11.4', true );
 	wp_enqueue_script( 'custom', THEME_JS . '/custom.js', array(), '6.2.4', true );
 }
 
