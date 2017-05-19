779dc71f8378d02706c6a9b1ed54a4fcba3819e3
Rich Edmunds <rich@webdevstudios.com>
2017-05-19 10:20:21 -0600

    Move critical CSS from header into enqueue with cookie.

--- themes/custom/functions/enqueue.php
+++ themes/custom/functions/enqueue.php
@@ -17,9 +17,14 @@
  */
 function custom_enqueue() {
 	// Styles.
-	// wp_enqueue_style( 'google-fonts', 'http://fonts.googleapis.com/css?family=Lato:300,400,700,900%7CHalant:400,600', array(), '1.0' );
 	// wp_enqueue_style( 'jquery-ui-css', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css', array(), '1.11.4' );
 	// wp_enqueue_style( 'custom', THEME_CSS . '/custom.css', array(), '1.0' );
+
+	// If the cookie is set, load our CSS normally.
+	if ( isset( $_COOKIE['custom-css'] ) && 'true' === $_COOKIE['custom-css'] ) {
+		wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700,900%7CHalant:400,600', array(), '1.0' );
+		wp_enqueue_style( 'custom', THEME_CSS . '/custom.css', array(), '1.0' );
+	}
 
 	// Scripts.
 	wp_deregister_script( 'jquery' );
@@ -29,3 +34,49 @@ function custom_enqueue() {
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_enqueue' );
+
+
+/**
+ * Critical CSS
+ *
+ * Include critical CSS and set cookie.
+ *
+ * @see https://codex.wordpress.org/Function_Reference/locate_template.
+ */
+function custom_theme_critical() {
+	// If we don't have a cookie set, load our inline styles and set cookie.
+	if ( ! isset( $_COOKIE['custom-css'] ) || 'true' !== $_COOKIE['custom-css'] ) :
+		?>
+		<style>
+			<?php locate_template( 'assets/scss/critical.css', true ); ?>
+		</style>
+		<link rel="preload" id="google-fonts-css" href="//fonts.googleapis.com/css?family=Lato:300,400,700,900%7CHalant:400,600" as="style" onload="this.rel='stylesheet'">
+		<link rel="preload" id="custom-css" href="/wp-content/themes/custom/assets/scss/custom.css" as="style" onload="this.rel='stylesheet'">
+		<script>
+			<?php locate_template( 'assets/js/critical.js', true ); ?>
+			// Set Cookie.
+			let expires = new Date( +new Date + ( 7 * 24 * 60 * 60 * 1000 ) ).toUTCString();
+			document.cookie = 'custom-css=true; expires=' + expires;
+		</script>
+		<?php
+	endif;
+}
+
+add_action( 'wp_head', 'custom_theme_critical', 30 );
+
+/**
+ * Noscript
+ *
+ * Include noscript in the footer.
+ */
+function custom_theme_noscript() {
+	// If cookie isn't set, load a noscript fallback.
+	if ( ! isset( $_COOKIE['custom-css'] ) || 'true' !== $_COOKIE['custom-css'] ) :
+		?>
+		<noscript><link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:300,400,700,900%7CHalant:400,600"></noscript>
+		<noscript><link rel="stylesheet" href="/wp-content/themes/custom/assets/scss/custom.css"></noscript>
+		<?php
+	endif;
+}
+
+add_action( 'wp_footer', 'custom_theme_noscript', 30 );
--- themes/custom/header.php
+++ themes/custom/header.php
@@ -13,20 +13,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '-', true, 'right' ); ?></title>
-	<style>
-		<?php include( 'assets/scss/critical.css' ); ?>
-	</style>
-
-	<link rel="preload" id="google-fonts-css" href="//fonts.googleapis.com/css?family=Lato:300,400,700,900%7CHalant:400,600" media="all" as="style" onload="this.rel='stylesheet'">
-	<noscript><link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:300,400,700,900%7CHalant:400,600" media="all"></noscript>
-
-	<link rel="preload" id="custom-css" href="/wp-content/themes/custom/assets/scss/custom.css" media="all" as="style" onload="this.rel='stylesheet'">
-	<noscript><link rel="stylesheet" href="/wp-content/themes/custom/assets/scss/custom.css" media="all"></noscript>
-
-	<script>
-		<?php include( 'assets/js/critical.js' ); ?>
-	</script>
-
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class( 'body-site' ); ?>>
