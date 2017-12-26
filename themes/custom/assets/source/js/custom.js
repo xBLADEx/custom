18edeae8900e35594e4e2b12fd2009620b56347b
Rich Edmunds <rich@richedmunds.com>
2017-12-25 21:46:58 -0700

    Update index entry file for scss and js, add custom js file.

---
+++ themes/custom/assets/source/js/custom.js
@@ -0,0 +1,27 @@
+//--------------------------------------------------------------
+// CUSTOM
+//--------------------------------------------------------------
+
+// Mobile Menu.
+var mainNav = $('.main-nav');
+
+$('.mobile-menu-icon').on('click', function(e) {
+	e.preventDefault();
+	mainNav.slideToggle();
+});
+
+// Slick Slider.
+// $( '.home-slider' ).slick({
+// 	dots: true,
+// 	infinite: true,
+// 	autoplay: false,
+// 	speed: 2000,
+// 	autoplaySpeed: 1000,
+// 	slidesToShow: 1,
+// 	slidesToScroll: 1,
+// 	pauseOnHover: false,
+// 	fade: false
+// });
+
+// Initialize Foundation.
+// $( document ).foundation();
--- themes/custom/assets/source/js/index.js
+++ themes/custom/assets/source/js/index.js
@@ -1,35 +1,9 @@
 //--------------------------------------------------------------
-// Custom jQuery
+// CUSTOM - INDEX
 //--------------------------------------------------------------
 
-( function( $ ) {
-	'use strict';
+// SCSS
+import '../scss/index.scss';
 
-	// Shorthand Document Ready.
-	$( function() {
-
-		// Mobile Menu.
-		var mainNav = $( '.main-nav' );
-
-		$( '.mobile-menu-icon' ).on( 'click', function( e ) {
-			e.preventDefault();
-			mainNav.slideToggle();
-		});
-
-		// Slick Slider.
-		// $( '.home-slider' ).slick({
-		// 	dots: true,
-		// 	infinite: true,
-		// 	autoplay: false,
-		// 	speed: 2000,
-		// 	autoplaySpeed: 1000,
-		// 	slidesToShow: 1,
-		// 	slidesToScroll: 1,
-		// 	pauseOnHover: false,
-		// 	fade: false
-		// });
-
-		// Initialize Foundation.
-		$( document ).foundation();
-	});
-}( jQuery ) );
+// JS
+import './custom';
