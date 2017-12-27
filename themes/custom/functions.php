bbb3fdd2a3022ed3a467086051c8e374a70a07ff
Rich Edmunds <rich@richedmunds.com>
2017-12-26 23:52:53 -0700

    Fix CSS path.

--- themes/custom/functions.php
+++ themes/custom/functions.php
@@ -7,7 +7,7 @@
 
 define( 'THEME_URI', get_template_directory_uri() );
 define( 'THEME_IMAGES', THEME_URI . '/assets/dist/images' );
-define( 'THEME_CSS', THEME_URI . '/assets/dist/scss' );
+define( 'THEME_CSS', THEME_URI . '/assets/dist/css' );
 define( 'THEME_JS', THEME_URI . '/assets/dist/js' );
 
 require get_parent_theme_file_path( 'functions/index.php' );
