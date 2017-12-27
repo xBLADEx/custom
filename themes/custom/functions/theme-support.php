8915c99135cfc8a6c462dd33328213af9936687b
Rich Edmunds <rich@richedmunds.com>
2017-12-26 23:04:25 -0700

    Hide ACF menu if not on local dev site.

--- themes/custom/functions/theme-support.php
+++ themes/custom/functions/theme-support.php
@@ -29,3 +29,28 @@ add_action( 'after_setup_theme', 'custom_support' );
  * Image Sizes
  */
 // add_image_size( 'name', 150, 80 );
+
+/**
+* Hide ACF Menu
+*
+* @author Rich Edmunds
+*/
+function base_hide_acf_admin() {
+	// Get the current site url
+	$site_url = get_bloginfo( 'url' );
+
+	$show_menu = [
+		'https://custom.test',
+	];
+
+	// If the url matches our dev url show the menu.
+	if ( in_array( $site_url, $show_menu ) ) {
+		// Show the acf menu item
+		return true;
+	} else {
+		// Hide the acf menu item
+		return false;
+	}
+}
+
+add_filter( 'acf/settings/show_admin', 'base_hide_acf_admin' );
