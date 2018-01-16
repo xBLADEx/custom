b8a31b5784ddc455796a08066c7736a2cbdc2bdb
Rich Edmunds <rich@richedmunds.com>
2018-01-16 11:38:45 -0700

    Add todo.

--- themes/custom/header.php
+++ themes/custom/header.php
@@ -12,7 +12,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php wp_title( '-', true, 'right' ); ?></title>
+	<title><?php wp_title( '-', true, 'right' ); ?></title> <?php // @todo Do we need this with yoast? I think not. ?>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class( 'body-site' ); ?>>
