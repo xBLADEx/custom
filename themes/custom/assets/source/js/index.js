6a6b532b22e9ec253e46bcb943c3556a8187312c
Rich Edmunds <rich@richedmunds.com>
2018-02-27 21:58:17 -0700

    Add lazysizes.

--- themes/custom/assets/source/js/index.js
+++ themes/custom/assets/source/js/index.js
@@ -5,5 +5,9 @@
 // SCSS
 import '../scss/index.scss';
 
+require('lazysizes/lazysizes'); // @see https://github.com/aFarkas/lazysizes
+require('lazysizes/plugins/parent-fit/ls.parent-fit'); // @see https://github.com/aFarkas/lazysizes#parent-fit-extension
+require('lazysizes/plugins/bgset/ls.bgset'); // @see https://github.com/aFarkas/lazysizes#bgset-plugin---lazy-responsive-background-image
+
 // JS
 import './custom';
