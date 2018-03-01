5b28b9113374647458755adca459e82e97b5a4a1
Rich Edmunds <rich@richedmunds.com>
2018-02-28 22:30:26 -0700

    Cleanup footer layout and styles.

--- themes/custom/footer.php
+++ themes/custom/footer.php
@@ -9,11 +9,9 @@
 
 </main>
 
-<footer class="footer">
-	<div class="row">
-		<div class="medium-12 columns">
-			<p><?php esc_html_e( 'Copyright &copy;', 'custom' ); ?> <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'All Rights Reserved.', 'custom' ); ?></p>
-		</div>
+<footer class="g-footer">
+	<div class="g-l-row">
+		<p><?php esc_html_e( 'Copyright &copy;', 'custom' ); ?> <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'All Rights Reserved.', 'custom' ); ?></p>
 	</div>
 </footer>
 
