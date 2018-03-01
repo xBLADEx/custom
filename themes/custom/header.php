b26cf01ec8ff0f99b0328f9f43b1b595c233deb5
Rich Edmunds <rich@richedmunds.com>
2018-02-28 22:46:22 -0700

    Organize header layout.

--- themes/custom/header.php
+++ themes/custom/header.php
@@ -15,22 +15,25 @@
 	<title><?php wp_title( '-', true, 'right' ); ?></title> <?php // @todo Do we need this with yoast? I think not. ?>
 	<?php wp_head(); ?>
 </head>
-<body <?php body_class( 'body-site' ); ?>>
-	<button class="mobile-menu-icon"><span><?php esc_html_e( 'Menu', 'custom' ); ?></span></button>
 
-	<header class="header-background">
-		<div class="row">
-			<div class="small-12 medium-3 columns">
-				<a href="/" class="logo"><?php get_template_part( 'assets/dist/images/logo.svg' ); ?></a>
+<body <?php body_class( 'body-site' ); ?>>
+	<header class="g-header">
+		<div class="g-l-row g-header__container">
+			<div class="g-header__logo-container">
+				<a href="<?php echo esc_url( home_url() ); ?>" class="g-header__logo"><?php get_template_part( 'assets/dist/images/logo.svg' ); ?></a>
 			</div>
 
-			<div class="medium-9 columns">
+			<div class="g-header__navigation">
 				<nav class="main-nav">
+					<button class="mobile-menu-icon"><span><?php esc_html_e( 'Menu', 'custom' ); ?></span></button>
 					<?php get_template_part( 'templates/menu-nav' ); ?>
 				</nav>
+			</div>
+
+			<div class="g-header__actions">
+				<?php custom_display_social_icons(); ?>
 
 				<?php get_template_part( 'templates/form-search' ); ?>
-				<?php custom_display_social_icons(); ?>
 			</div>
 		</div>
 	</header>
