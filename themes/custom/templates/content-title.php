e9de1814a7dacbdaf6c471d6fdd56543e9385e28
Rich Edmunds <rich@richedmunds.com>
2018-03-11 10:46:36 -0600

    Remove foundation markup and class.

--- themes/custom/templates/content-title.php
+++ themes/custom/templates/content-title.php
@@ -6,16 +6,14 @@
  */
 
 ?>
-<div class="row">
-	<div class="medium-12 columns">
-		<h1 class="page-title">
-			<?php
-			if ( is_archive() ) {
-				post_type_archive_title();
-			} else {
-				the_title();
-			}
-			?>
-		</h1>
-	</div>
+<div class="g-l-row">
+	<h1 class="page-title">
+		<?php
+		if ( is_archive() ) {
+			post_type_archive_title();
+		} else {
+			the_title();
+		}
+		?>
+	</h1>
 </div>
