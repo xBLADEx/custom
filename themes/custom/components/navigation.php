4d80c83c20bd9640a7ffefb8d3cce57f8036923a
Rich Edmunds <rich@richedmunds.com>
2018-04-15 20:14:48 -0600

    Rename to main menu, register only one sidebar.

--- themes/custom/components/navigation.php
+++ themes/custom/components/navigation.php
@@ -6,23 +6,21 @@
  * @package Custom
  */
 
-wp_nav_menu(
-	array(
-		'theme_location'  => 'main_nav',
-		'menu'            => '',
-		'container'       => false,
-		'container_class' => '',
-		'container_id'    => '',
-		'menu_class'      => '',
-		'menu_id'         => '',
-		'echo'            => true,
-		'fallback_cb'     => false,
-		'before'          => '',
-		'after'           => '',
-		'link_before'     => '',
-		'link_after'      => '',
-		'items_wrap'      => '<ul class="navigation__menu">%3$s</ul>',
-		'depth'           => 2,
-		'walker'          => new Custom_Nav_Menu(),
-	)
-);
+wp_nav_menu( [
+	'theme_location'  => 'main_menu',
+	'menu'            => '',
+	'container'       => false,
+	'container_class' => '',
+	'container_id'    => '',
+	'menu_class'      => '',
+	'menu_id'         => '',
+	'echo'            => true,
+	'fallback_cb'     => false,
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '<ul class="navigation__menu">%3$s</ul>',
+	'depth'           => 2,
+	'walker'          => new Custom_Nav_Menu(),
+] );
--- themes/custom/functions/register.php
+++ themes/custom/functions/register.php
@@ -9,47 +9,17 @@
  * Sidebars
  */
 function custom_widgets() {
-	// Sidebar Right.
-	register_sidebar(
-		array(
-			'name'          => __( 'Sidebar Right', 'custom' ),
-			'id'            => 'sidebar-right',
-			'description'   => '',
-			'class'         => 'sidebar-right',
-			'before_widget' => '<div class="sidebar-widget">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h5>',
-			'after_title'   => '</h5>',
-		)
-	);
-
-	// Sidebar Left.
-	register_sidebar(
-		array(
-			'name'          => __( 'Sidebar Left', 'custom' ),
-			'id'            => 'sidebar-left',
-			'description'   => '',
-			'class'         => 'sidebar-left',
-			'before_widget' => '<div class="sidebar-widget">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h5>',
-			'after_title'   => '</h5>',
-		)
-	);
-
-	// Sidebar Blog.
-	register_sidebar(
-		array(
-			'name'          => __( 'Sidebar Blog', 'custom' ),
-			'id'            => 'sidebar-blog',
-			'description'   => '',
-			'class'         => 'sidebar-blog',
-			'before_widget' => '<div class="sidebar-widget">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h5>',
-			'after_title'   => '</h5>',
-		)
-	);
+	// Sidebar.
+	register_sidebar( [
+		'name'          => __( 'Sidebar', 'custom' ),
+		'description'   => '',
+		'id'            => 'sidebar',
+		'class'         => 'sidebar',
+		'before_widget' => '<div class="sidebar-widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h5>',
+		'after_title'   => '</h5>',
+	] );
 }
 
 add_action( 'widgets_init', 'custom_widgets' );
@@ -59,9 +29,6 @@ add_action( 'widgets_init', 'custom_widgets' );
  *
  * @see http://codex.wordpress.org/Function_Reference/register_nav_menus.
  */
-register_nav_menus(
-	array(
-		'main_nav'      => __( 'Main Navigation', 'custom' ),
-		'secondary_nav' => __( 'Secondary Navigation', 'custom' ),
-	)
-);
+register_nav_menus( [
+	'main_menu' => __( 'Main Menu', 'custom' ),
+] );
