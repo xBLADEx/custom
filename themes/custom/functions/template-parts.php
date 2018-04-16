414157c4fb7e6ec8d7b0a9d9e6c7fa86cf91af21
Rich Edmunds <rich@richedmunds.com>
2018-04-15 20:16:57 -0600

    Remove commented code, adjust file comment.

--- themes/custom/functions/template-parts.php
+++ themes/custom/functions/template-parts.php
@@ -1,26 +1,10 @@
 <?php
 /**
  * Template Parts
- * Theme function files for templates.
+ * Theme functions for templates.
  *
  * @package Base
  */
-
-/**
- * Shortcode Example
- *
- * @example [name].
- * @return string HTML.
- */
-/* function custom_shortcode_name() {
-	ob_start();
-	?>
-
-	<?php
-	return ob_get_clean();
-}
-
-add_shortcode( 'name', 'custom_shortcode_name' ); */
 
 /**
  * Hero Content
