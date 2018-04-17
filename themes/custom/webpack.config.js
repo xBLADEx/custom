bf27d7b9ddd34f550c3775ce6297baddc7992e28
Rich Edmunds <rich@richedmunds.com>
2018-04-17 13:11:44 -0600

    Replace extract text for mini css extract for webpack 4.0.

--- themes/custom/webpack.config.js
+++ themes/custom/webpack.config.js
@@ -6,7 +6,7 @@
 
 const path = require('path'); // Needed for webpack to output path.
 const webpack = require('webpack'); // Needed for ProvidePlugin to import jQuery.
-const ExtractTextPlugin = require('extract-text-webpack-plugin'); // Separates files from JS. Example creates a .css file.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Extract CSS into separate files.
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin'); // Browsersync.
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // Minify JS.
 const CopyWebpackPlugin = require('copy-webpack-plugin'); // Copies individual files or entire directories to the build directory.
@@ -17,36 +17,6 @@ module.exports = env => {
 	const isProduction = true === env.production; // Create boolean true if in production mode.
 
 	let sourceMap = isProduction ? '' : 'inline-source-map'; // Set inline source map if not in production.
-
-	// Setup Sass loaders for dev build.
-	const sassLoaders = [
-		{
-			loader: 'css-loader', // @see https://webpack.js.org/loaders/css-loader/
-			options: {
-				sourceMap: !isProduction, // @see https://github.com/webpack-contrib/sass-loader#source-maps
-				importLoaders: 1, // 0 => no loaders (default); 1 => postcss-loader; 2 => postcss-loader, sass-loader
-				url: false, // @todo, we need to figure out proper url() pathing for fonts.
-			},
-		},
-		{
-			loader: 'sass-loader',
-			options: {
-				sourceMap: !isProduction, // @see https://github.com/webpack-contrib/sass-loader#source-maps
-				outputStyle: 'compact', // @see https://github.com/sass/node-sass#outputstyle. Note: Currently required due to node-sass & bug https://github.com/webpack-contrib/sass-loader/issues/351
-			},
-		},
-	];
-
-	// Is Production.
-	if (isProduction) {
-		// Add PostCSS.
-		sassLoaders.splice(1, 0, {
-			loader: 'postcss-loader', // @see https://webpack.js.org/loaders/postcss-loader/. Note order is important https://webpack.js.org/loaders/postcss-loader/#config-cascade.
-			// options: {
-			// 	sourceMap: ! isProduction // @see https://webpack.js.org/loaders/postcss-loader/#usage
-			// }
-		});
-	}
 
 	return {
 		entry: {
@@ -74,10 +44,28 @@ module.exports = env => {
 					],
 				},
 				{
-					test: /\.scss$/,
-					use: ExtractTextPlugin.extract({
-						use: sassLoaders,
-					}),
+					test: /\.(scss|css)$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader', // @see https://webpack.js.org/loaders/css-loader/
+							options: {
+								sourceMap: !isProduction, // @see https://github.com/webpack-contrib/sass-loader#source-maps
+								importLoaders: 1, // 0 => no loaders (default); 1 => postcss-loader; 2 => postcss-loader, sass-loader
+								url: false, // @todo, we need to figure out proper url() pathing for fonts.
+							},
+						},
+						{
+							loader: 'postcss-loader', // @see https://webpack.js.org/loaders/postcss-loader/.
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: !isProduction, // @see https://github.com/webpack-contrib/sass-loader#source-maps
+								outputStyle: 'compact', // @see https://github.com/sass/node-sass#outputstyle. Note: Currently required due to node-sass & bug https://github.com/webpack-contrib/sass-loader/issues/351
+							},
+						},
+					],
 				},
 				{
 					test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, // Fonts
@@ -121,19 +109,11 @@ module.exports = env => {
 				$: 'jquery',
 				jQuery: 'jquery',
 			}),
-			new ExtractTextPlugin('css/[name].css'), // [name] comes from entry.property. In our case 'custom'.css.
+			new MiniCssExtractPlugin({ filename: 'css/[name].css' }), // @see https://github.com/webpack-contrib/mini-css-extract-plugin
 			new UglifyJSPlugin({
 				// @see https://www.npmjs.com/package/uglifyjs-webpack-plugin
 				test: /\.js$/i,
 				sourceMap: !isProduction,
-				compress: {
-					drop_console: isProduction,
-					warnings: false,
-				},
-				output: {
-					beautify: !isProduction,
-					comments: !isProduction,
-				},
 			}),
 			new BrowserSyncPlugin({
 				proxy: `https://${proxy}`,
