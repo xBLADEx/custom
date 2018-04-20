fca28ef9c9646f5fd7729df985a0f30bc12b3e6a
Rich Edmunds <rich@richedmunds.com>
2018-04-19 19:38:00 -0600

    Remove old jQuery code.

--- themes/custom/assets/source/js/custom.js
+++ themes/custom/assets/source/js/custom.js
@@ -1,11 +1,3 @@
 //--------------------------------------------------------------
 // CUSTOM
 //--------------------------------------------------------------
-
-const mainNav = $('.main-nav');
-const menuIcon = $('.mobile-menu-icon');
-
-menuIcon.on('click', e => {
-	e.preventDefault();
-	mainNav.slideToggle();
-});
