78e7a22dc1dfec522d08040621978efc1d9aa977
Rich Edmunds <rich@richedmunds.com>
2018-09-14 16:07:59 -0600

    Clean up footer markup and styles.

--- themes/custom/footer.php
+++ themes/custom/footer.php
@@ -11,7 +11,7 @@
 
 <footer class="g-footer">
 	<div class="g-l-row">
-		<p><?php esc_html_e( 'Copyright &copy;', 'custom' ); ?> <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'All Rights Reserved.', 'custom' ); ?></p>
+		<p class="g-footer__copyright">&copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'All Rights Reserved.', 'custom' ); ?></p>
 	</div>
 </footer>
 
