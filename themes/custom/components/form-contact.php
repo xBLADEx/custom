8ec81d69f13de44e3817a19631f63de0a7fab44a
Rich Edmunds <rich@richedmunds.com>
2018-09-26 15:42:47 -0600

    Update form and logic, add nonce verification.

--- themes/custom/components/form-contact.php
+++ themes/custom/components/form-contact.php
@@ -5,67 +5,59 @@
  * @package Custom
  */
 
-if ( isset( $_POST['submit'] ) ) :
-	// Define emails, website, and domain.
-	define( 'EMAIL',   'javablood@hotmail.com' );
-	define( 'WEBSITE', get_bloginfo( 'name' ) );
-	define( 'DOMAIN',  str_replace( 'www.', '', $_SERVER['HTTP_HOST'] ) );
-
+if ( isset( $_POST['submit'] ) && ! wp_verify_nonce( 'custom_form_contact_name', 'custom_form_contact_action' ) ) :
 	// If trap value is set redirect to home page.
-	if ( '' !== $_POST['main_address'] ) {
+	if ( '' !== sanitize_text_field( wp_unslash( $_POST['main_address'] ) ) ) {
 		echo '<html><head><meta http-equiv="refresh" content="0; url=/"></head><body></body></html>';
-
 		exit;
 	}
 
-	// Set required fields.
-	$required_fields = array(
-		'full_name',
-		'email',
-		'phone',
-	);
+	// Define emails, website, and domain.
+	define( 'EMAIL', get_field( 'global_form_email', 'options' ) );
+	define( 'WEBSITE', get_bloginfo( 'name' ) );
+	define( 'DOMAIN', str_replace( 'www.', '', $_SERVER['HTTP_HOST'] ) );
 
-	// Check if required fields are blank.
-	$field_errors = true;
-
-	foreach ( $required_fields as &$field ) {
-		if ( '' === $_POST[ $field ] ) {
-			$field_errors = false;
-		}
-	}
-
-	// Grab form fields.
-	$full_name = $_POST['full_name'];
+	// Fields.
+	$full_name = wp_unslash( $_POST['full_name'] );
 	$email     = $_POST['email'];
 	$phone     = $_POST['phone'];
 	$comment   = $_POST['comment'];
+
+	// Set required fields.
+	$required_fields = [
+		'full_name',
+		'email',
+	];
+
+	// Check if required fields are blank.
+	$field_errors = false;
+
+	foreach ( $required_fields as $field ) {
+		if ( '' === $_POST[ $field ] ) {
+			$field_errors = true;
+		}
+	}
 
 	// Sanitize Information.
 	if ( '' !== $full_name ) {
 		$full_name = filter_var( $full_name, FILTER_SANITIZE_STRING );
 	}
+
 	if ( '' !== $email ) {
 		$email = filter_var( $email, FILTER_SANITIZE_EMAIL );
 	}
+
 	if ( '' !== $phone ) {
 		$regex = '/([0-9]{3})\.?([0-9]{3})\.?([0-9]{4})/';
 		$phone = preg_replace( $regex, '$1-$2-$3', $phone );
 	}
 
-	// Check if theme options email field is set.
-	if ( '' === EMAIL ) {
-		$to = 'javablood@hotmail.com';
-	} else {
-		$to = EMAIL;
-	}
-
-	$subject = WEBSITE . ' Contact Form' . "\r\n";
-
-	$headers = 'MIME-Version: 1.0' . "\r\n";
+	$to       = EMAIL;
+	$subject  = WEBSITE . ' Contact Form' . "\r\n";
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 	$headers .= 'From: contact@' . DOMAIN . "\r\nReply-To: noreply@" . DOMAIN . "\r\n";
-
-	$message = '<html><body>';
+	$message  = '<html><body>';
 	$message .= '<table rules="all" style="border-color: #B8B8B8;" cellpadding="10">';
 	$message .= '<tr style="background: #5979FF; color: #FFF; font-weight: bold;"><th>FIELD</th><th>INFORMATION</th></tr>';
 	$message .= '<tr><td><strong>Name:</strong></td><td>' . strip_tags( $full_name ) . '</td></tr>';
@@ -79,19 +71,20 @@ if ( isset( $_POST['submit'] ) ) :
 	$message = wordwrap( $message, 70 );
 
 	// If no errors - send. Else - alert.
-	if ( $field_errors ) :
+	if ( ! $field_errors ) :
 		wp_mail( $to, $subject, $message, $headers );
 		?>
-		<p class="alert-box success"><?php esc_html_e( 'Thank you for contacting us!', 'custom' ); ?></p>
+		<p class="c-notification-box c-notification-box--success"><?php esc_html_e( 'Thank you for contacting us!', 'custom' ); ?></p>
 		<?php
 	else :
 		?>
-		<p class="alert-box alert"><?php esc_html_e( 'Please fill out the required fields.', 'custom' ); ?></p>
+		<p class="c-notification-box c-notification-box--error"><?php esc_html_e( 'Please fill out all the required fields.', 'custom' ); ?></p>
 		<?php
 	endif;
 else :
 	?>
 	<form method="POST" class="form form--contact">
+		<?php wp_nonce_field( 'custom_form_contact_action', 'custom_form_contact_name' ); ?>
 		<div class="form__section form__group">
 			<label for="form-full-name"><?php esc_html_e( 'Full Name *', 'custom' ); ?></label>
 			<input type="text" name="full_name" id="form-full-name" class="form__input" placeholder="<?php esc_attr_e( 'Full Name', 'custom' ); ?>" required>
@@ -104,7 +97,7 @@ else :
 
 		<div class="form__section form__group">
 			<label for="form-phone"><?php esc_html_e( 'Phone', 'custom' ); ?></label>
-			<input type="tel" name="phone" id="form-phone" class="form__input" maxlength="12" pattern="\d{3}[\-]?\d{3}[\-]?\d{4}" placeholder="<?php esc_attr_e( '555-555-5555', 'custom' ); ?>" required>
+			<input type="tel" name="phone" id="form-phone" class="form__input" maxlength="12" pattern="\d{3}[\-]?\d{3}[\-]?\d{4}" placeholder="<?php esc_attr_e( '555-555-5555', 'custom' ); ?>">
 		</div>
 
 		<div class="form__section form__group">
