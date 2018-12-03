999cd1258421a4174cc8e0f5183c955a574b9ca5
Rich Edmunds <rich@richedmunds.com>
2018-12-03 08:59:49 -0700

    Update page hero, add acf fields.

--- themes/custom/functions/template-parts.php
+++ themes/custom/functions/template-parts.php
@@ -20,32 +20,18 @@ function custom_display_hero_content( $title = '' ) {
 
 	$page_hero_image = get_field( 'page_hero_image' );
 	$page_title      = get_field( 'page_title' );
-	$page_sub_title  = get_field( 'page_sub_title' );
-	$page_link       = get_field( 'page_link' );
 
 	// Set a hero fallback image.
 	$page_hero_image = $page_hero_image ? $page_hero_image : get_field( 'global_hero_image', 'options' );
 
 	// Is title set in the function, use it, else use the ACF field, fallback to the post title.
 	$title = $title ? $title : ( $page_title ? $page_title : get_the_title() );
-
-	// Get the page link name.
-	$link_id    = url_to_postid( $page_link );
-	$link_title = get_the_title( $link_id );
 	?>
 	<div class="g-page-header">
 		<div <?php custom_the_image_background_acf( $page_hero_image, 'hero-small', [ 'g-page-header__background' ] ); ?>></div>
 
 		<div class="g-l-row">
 			<h1><?php echo esc_html( $title ); ?></h1>
-
-			<?php if ( $page_sub_title ) : ?>
-				<h2><?php echo esc_html( $page_sub_title ); ?></h2>
-			<?php endif; ?>
-
-			<?php if ( $page_link ) : ?>
-				<a href="<?php echo esc_url( $page_link ); ?>" class="button"><?php echo esc_html( $link_title ); ?></a>
-			<?php endif; ?>
 		</div>
 	</div>
 	<?php
