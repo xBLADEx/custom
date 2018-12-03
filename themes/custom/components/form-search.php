6782c943a9ce9edcb4f394623463eb15d83d8927
Rich Edmunds <rich@richedmunds.com>
2018-12-03 09:31:00 -0700

    Fix :focus to come before :hover, adjust social and form.

--- themes/custom/components/form-search.php
+++ themes/custom/components/form-search.php
@@ -10,7 +10,13 @@
 <form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get" class="form-search">
 	<label>
 		<span class="screen-reader-text"><?php esc_html_e( 'Search for:', 'custom' ); ?></span>
-		<input type="search" name="s" class="form-search__input" placeholder="<?php esc_attr_e( 'Search', 'custom' ); ?>" value="<?php echo get_search_query(); ?>">
+		<input
+			type="search"
+			name="s"
+			class="form-search__input"
+			placeholder="<?php esc_attr_e( 'Search', 'custom' ); ?>"
+			value="<?php echo get_search_query(); ?>"
+		>
 	</label>
 
 	<input type="submit" class="button form-search__submit" value="<?php esc_attr_e( 'Go', 'custom' ); ?>">
