ae7c8f259ae4575c785200f83aa4ff931331f80c
Rich Edmunds <rich@richedmunds.com>
2019-02-11 09:52:14 -0700

    Add menu overlay.

--- themes/custom/assets/source/js/menu.js
+++ themes/custom/assets/source/js/menu.js
@@ -7,6 +7,22 @@ const menu = document.querySelector('[data-js-menu]');
 
 function toggleMenu() {
 	menu.classList.toggle('is-active');
+
+	// Overlay
+	overlay();
+}
+
+function overlay() {
+	// Create and append.
+	const overlay = document.createElement('span');
+	overlay.classList.add('navigation__overlay');
+	document.querySelector('.navigation').appendChild(overlay);
+
+	// Remove and close menu.
+	overlay.addEventListener('click', _ => {
+		overlay.remove();
+		menu.classList.remove('is-active');
+	});
 }
 
 if (menuTrigger && menu) {
