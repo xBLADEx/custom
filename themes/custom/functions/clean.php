5701c56125e9b7c01488a75e47108ea7067fb724
Rich Edmunds <rich@richedmunds.com>
2019-10-08 10:42:07 -0600

    Remove post filters for images.
    The new editor seems to have improved what we were removing.

--- themes/custom/functions/clean.php
+++ themes/custom/functions/clean.php
@@ -15,18 +15,17 @@ function custom_clean_header() {
 	remove_action( 'wp_head', 'feed_links_extra', 3 ); // Removes the type="application/rss+xml" /feed link.
 	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 ); // Remove shortlink.
 	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 ); // Removes rel='prev' and rel='next'.
+
 	// 4.2 WordPress Emoji.
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' ); // Admin
 	remove_action( 'admin_print_styles', 'print_emoji_styles' ); // Admin
-	// Post Related.
-	add_filter( 'get_image_tag_class', 'custom_image_classes', 0, 4 );
-	add_filter( 'get_image_tag', 'custom_image_remove_sizes', 0, 4 );
-	add_filter( 'image_send_to_editor', 'custom_image_to_relative', 5, 8 );
+
 	// Remove Version
 	add_filter( 'style_loader_src', 'custom_remove_cssjs_ver', 10, 2 );
 	add_filter( 'script_loader_src', 'custom_remove_cssjs_ver', 10, 2 );
+
 	// Widgets Comments
 	add_action( 'widgets_init', 'custom_remove_recent_comments_style' );
 
@@ -59,71 +58,4 @@ function custom_remove_recent_comments_style() {
 	global $wp_widget_factory;
 
 	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
-}
-
-/**
- * Remove All Class Names Except Alignment
- *
- * @param  string $class Class.
- * @param  string $id    ID.
- * @param  string $align Align.
- * @param  string $size  Size.
- * @return string        New image tag.
- */
-function custom_image_classes( $class, $id, $align, $size ) {
-	$align = 'align' . esc_attr( $align );
-
-	return $align;
-}
-
-/**
- * Remove Image Attributes Width And Height
- * Remove width and height on insert image into post.
- *
- * @param  string $html  HTML.
- * @param  string $id    ID.
- * @param  string $alt   Alt.
- * @param  string $title New inserted image without size attributes.
- */
-function custom_image_remove_sizes( $html, $id, $alt, $title ) {
-	return preg_replace(
-		[
-			'/\s+width="\d+"/i',
-			'/\s+height="\d+"/i',
-			'/alt=""/i',
-		],
-		[
-			'',
-			'',
-			'',
-			'alt="' . $title . '"',
-		],
-		$html
-	);
-}
-
-/**
- * Image SRC Attribute Relative Path
- * Remove absolute URL on insert image into post.
- *
- * @param  string $html    HTML.
- * @param  string $id      ID.
- * @param  string $caption Caption.
- * @param  string $title   Title.
- * @param  string $align   Align.
- * @param  string $url     URL.
- * @param  string $size    Size.
- * @param  string $alt     Alt.
- * @return string          New image inserted clean.
- */
-function custom_image_to_relative( $html, $id, $caption, $title, $align, $url, $size, $alt ) {
-	$sp          = strpos( $html, 'src=' ) + 5;
-	$ep          = strpos( $html, '\"', $sp );
-	$imageurl    = substr( $html, $sp, $ep - $sp );
-	$relativeurl = str_replace( 'http://', '', $imageurl );
-	$sp          = strpos( $relativeurl, '/' );
-	$relativeurl = substr( $relativeurl, $sp );
-	$html        = str_replace( $imageurl, $relativeurl, $html );
-
-	return $html;
 }
