c939a519e5c0e046af46e22d0a6e582492be1873
Rich Edmunds <rich@richedmunds.com>
2019-12-03 10:51:39 -0700

    Update button block styles, add package for lazyload to be ready for IE.

--- themes/custom/assets/source/js/index.js
+++ themes/custom/assets/source/js/index.js
@@ -8,6 +8,7 @@ import '../scss/index.scss';
 require('lazysizes/lazysizes'); // @see https://github.com/aFarkas/lazysizes
 require('lazysizes/plugins/parent-fit/ls.parent-fit'); // @see https://github.com/aFarkas/lazysizes#parent-fit-extension
 require('lazysizes/plugins/bgset/ls.bgset'); // @see https://github.com/aFarkas/lazysizes#bgset-plugin---lazy-responsive-background-image
+// require('lazysizes/plugins/respimg/ls.respimg'); // IE 11
 
 // JS
 // import './slick-slider';
