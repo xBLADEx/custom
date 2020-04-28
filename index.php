02fea051e9710db4dfdf888cb9d9c5e1f088c41a
Rich Edmunds <rich@richedmunds.com>
2020-04-28 16:43:10 -0600

    Move repo file structure.

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-// Silence is golden.
+/**
+ * Index
+ *
+ * @package Custom
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+?>
+
+<div class="g-row">
+	<?php
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
+			the_content();
+		}
+	}
+	?>
+</div>
+
+<?php get_footer(); ?>
