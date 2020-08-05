875ccc72b782fb9482dbed81f55d3a3af243b447
Rich Edmunds <rich@richedmunds.com>
2020-08-05 10:28:46 -0600

    Add extends, lint file.

--- assets/source/js/menu.js
+++ assets/source/js/menu.js
@@ -6,29 +6,29 @@ const menuTrigger = document.querySelector('[data-js-menu-trigger]');
 const menu = document.querySelector('[data-js-menu]');
 
 function toggleMenu() {
-	// Convert attribute to boolean.
-	const open = JSON.parse(menuTrigger.getAttribute('aria-expanded'));
-	menuTrigger.setAttribute('aria-expanded', !open);
-	menu.classList.toggle('is-active');
+  // Convert attribute to boolean.
+  const open = JSON.parse(menuTrigger.getAttribute('aria-expanded'));
+  menuTrigger.setAttribute('aria-expanded', !open);
+  menu.classList.toggle('is-active');
 
-	// Overlay
-	overlay();
+  // Overlay
+  overlay();
 }
 
 function overlay() {
-	// Create and append.
-	const overlay = document.createElement('span');
-	overlay.classList.add('navigation__overlay');
-	document.querySelector('.navigation').appendChild(overlay);
+  // Create and append.
+  const overlay = document.createElement('span');
+  overlay.classList.add('navigation__overlay');
+  document.querySelector('.navigation').appendChild(overlay);
 
-	// Remove and close menu.
-	overlay.addEventListener('click', _ => {
-		overlay.remove();
-		menuTrigger.setAttribute('aria-expanded', !open);
-		menu.classList.remove('is-active');
-	});
+  // Remove and close menu.
+  overlay.addEventListener('click', () => {
+    overlay.remove();
+    menuTrigger.setAttribute('aria-expanded', !open);
+    menu.classList.remove('is-active');
+  });
 }
 
 if (menuTrigger && menu) {
-	menuTrigger.addEventListener('click', toggleMenu);
+  menuTrigger.addEventListener('click', toggleMenu);
 }
