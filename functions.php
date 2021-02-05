b8fee0e630549e3b23a552c25d9d659f54f7da1b
Rich Edmunds <rich@richedmunds.com>
2021-02-05 16:41:18 -0700

    perf: Update google fonts url

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ define( 'THEME_CSS', THEME_URI . '/assets/dist/css' );
 define( 'THEME_JS', THEME_URI . '/assets/dist/js' );
 define( 'VERSION_CSS', filemtime( get_template_directory() . '/assets/dist/css/custom.css' ) );
 define( 'VERSION_JS', filemtime( get_template_directory() . '/assets/dist/js/custom.js' ) );
-define( 'GOOGLE_FONTS', '//fonts.googleapis.com/css?family=Lato:300,400,700%7CMontserrat:300,400,700&display=swap' );
+define( 'GOOGLE_FONTS', '//fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Montserrat:wght@300;400;700&display=swap' );
 define( 'RECAPTCHA_SITE_KEY', '' );
 define( 'RECAPTCHA_SECRET_KEY', '' );
 
